a78e7a68de0180aae7a504f4e4b811a44c4ee9a1
denjayx <denyw602@gmail.com>
2023-10-31 18:24:17 +0700

    wip: offline capability

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,8 +1,10 @@
 import Detail from '../views/pages/detail';
+import Favourite from '../views/pages/favourtie';
 import RestoList from '../views/pages/resto';
 
 const routes = {
   '/': RestoList,
+  '/favourite': Favourite,
   '/detail/:id': Detail,
 };
 
