8a5e95f8c0242975d2e6e3a2440cd1f78565b068
denjayx <denyw602@gmail.com>
2023-11-17 23:39:04 +0700

    fix: preload cdn

--- webpack.common.js
+++ webpack.common.js
@@ -92,16 +92,16 @@ module.exports = {
     splitChunks: {
       chunks: 'all',
       minSize: 20000,
-      maxSize: 70000,
+      minRemainingSize: 0,
       minChunks: 1,
       maxAsyncRequests: 30,
       maxInitialRequests: 30,
-      automaticNameDelimiter: '~',
       enforceSizeThreshold: 50000,
       cacheGroups: {
         defaultVendors: {
           test: /[\\/]node_modules[\\/]/,
           priority: -10,
+          reuseExistingChunk: true,
         },
         default: {
           minChunks: 2,
